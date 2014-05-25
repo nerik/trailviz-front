421f4ea9b04a561a0f9794ec6ad35caeefbad793
nerik <erik.escoffier@gmail.com>
2014-05-25 23:16:42 +0200

    auth works

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,27 @@
-console.log('hello front')
 
-$.ajax({
-  url: "http://127.0.0.1:8000/"
-}).done(function( data ) {
-    console.log(data)
-});
+var params = window.location.search.replace( "?", "" );
+var token = '';
+var hgUrl = 'http://api.runkeeper.com'
+
+if (params !== '') {
+	$.ajax({
+	  url: "http://127.0.0.1:8000/?" + params
+	}).done(function( data ) {
+	    console.log(data);
+	    var resp = JSON.parse(data);
+	    console.log(resp.access_token);
+	    if (resp.access_token) {
+	    	token = resp.access_token;
+	    	fetchUser();
+	    }
+	});
+}
+
+function fetchUser () {
+	console.log('fetch!');
+	console.log(token);
+	$.ajax({url: hgUrl + '/user?access_token=' + token}).done(function (data) {
+		console.log(data);
+	})
+	  
+}
